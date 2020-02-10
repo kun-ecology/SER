8f9a1d4e6d52dcbfe82b514bcb55c7fbcb711ff6
tequierogk <mlngm@hotmail.com>
2020-02-10 12:19:32 +0100

    update

---
+++ R/hydro-data.R
@@ -0,0 +1,16 @@
+#' Discharge data in a stream
+#'
+#' @docType data
+#'
+#'
+#' @format A dataframe
+#'    \describe{
+#'       \item{Date}{Date data, from 2016 to 2018}
+#'       \item{Discharge}{Discharge data (L/s), daily discharge data in a stream}
+#'    }
+#' @keywords datasets
+#'
+#' @source
+#'
+
+"hydro_df"
---
+++ R/sample_date-data.R
@@ -0,0 +1,9 @@
+#' Sample dates (vector)
+#'
+#'
+#' @docType data
+#'
+#' @format A vector containg 13 dates data
+
+
+"sample_date"
